080164b4cb00ac0f80792f322675e4f41bf1646e
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2022-01-27 10:00:34 +0100

    started

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace review_evaluation_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
