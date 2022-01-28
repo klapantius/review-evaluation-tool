9e71f2f5c4750925e31eef9eb4868b8300b2e58a
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2022-01-28 07:41:39 +0100

    introduce mockable parameter processing

---
+++ OptionProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace review_evaluation_tool
+{
+    interface IOptionProvider
+    {
+        string GetParam(string envVarName, string optionName, string defaultValue = null);
+        bool IsSpecified(string envVarName, string optionName);
+    }
+
+    internal class OptionProvider : IOptionProvider
+    {
+        private readonly List<string> args;
+
+        public OptionProvider(string[] args)
+        {
+            this.args = args.Select(a => a.ToLowerInvariant().Trim('-', '/')).ToList();
+        }
+
+        public string GetParam(string envVarName, string optionName, string defaultValue = null)
+        {
+            var env = Environment.GetEnvironmentVariable(envVarName);
+            if (!string.IsNullOrEmpty(env))
+            {
+                return env;
+            }
+            if (!string.IsNullOrWhiteSpace(optionName))
+            {
+            var idx = args.IndexOf(optionName.ToLowerInvariant());
+            if (idx >= 0)
+            {
+                if (idx < args.Count) return args[idx + 1];
+                throw new Exception($"Argument \"{optionName}\" has no value");
+            }
+            }
+            if (!string.IsNullOrWhiteSpace(defaultValue))
+            {
+                return defaultValue;
+            }
+            throw new Exception($"Neither environment variable \"{envVarName}\" nor cmd line argument \"{optionName}\" is available.");
+        }
+
+        public bool IsSpecified(string envVarName, string optionName)
+        {
+            var env = Environment.GetEnvironmentVariable(envVarName);
+            if (!string.IsNullOrWhiteSpace(env))
+            {
+                if (bool.TryParse(envVarName, out var result))
+                {
+                    return result;
+                }
+                throw new Exception($"Environment variable \"{envVarName}\" is not a boolean.");
+            }
+            var idx = args.IndexOf(optionName.ToLowerInvariant());
+            if (idx >= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
