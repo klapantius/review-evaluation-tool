eaa2cc84d3a99e1b0819fe64f67e2b3cd35e4664
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2022-01-28 15:56:04 +0100

    add feature of evaluating count of reviewers

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Text.Json;
 using System.Diagnostics;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.TeamFoundation.SourceControl.WebApi;
 using System.Text;
 
@@ -26,7 +25,6 @@ namespace review_evaluation_tool
             var tp = opt.GetParam("BUILD_TEAMPROJECT", "tp", "syngo.net");
             var buildId = int.Parse(opt.GetParam("BUILD_BUILDID", "buildid", "0"));
             var prId = int.Parse(opt.GetParam(null, "prid", "0"));
-            var repoName = string.Empty;
 
             var tpc = new ConnectionProvider().Connect();
             var buildClient = tpc.GetClient<BuildHttpClient>();
@@ -37,15 +35,10 @@ namespace review_evaluation_tool
                 var buildData = await buildClient.GetBuildAsync(project: tp, buildId);
                 var buildParams = JsonSerializer.Deserialize<Dictionary<string, string>>(buildData.Parameters);
                 Console_Dump($"parameters of build {buildData.BuildNumber}", buildParams);
-
                 prId = int.Parse(buildParams["system.pullRequest.pullRequestId"]);
-                var repoUri = buildParams["system.pullRequest.sourceRepositoryUri"];
-                repoName = repoUri.Split('/').Last();
             }
-            else
-            {
-                if (prId == 0) throw new Exception("Neither a build id nor a PR id is available to start with.");
-            }
+            // at this point we should have a PR id
+            if (prId == 0) throw new Exception("No PR could be identified.");
 
             Console.WriteLine($"loading PR {prId}");
             var pr = await git.GetPullRequestByIdAsync(project: tp, prId);
@@ -55,15 +48,28 @@ namespace review_evaluation_tool
             Console.WriteLine($"safety relevant: {isSafety}");
             if (!isSafety) return 0;
 
-            var reviewers = pr.Reviewers;
-            Console_Dump("reviewers", reviewers.Select(r => new { r.DisplayName, _ = r.IsRequired ? "required" : "optional", r.Vote }));
+            var rc = new ReviewerCounter(git, 2);
+            if (await rc.Check(pr))
+            {
+                Console.WriteLine("Minimum number of reviewers is ensured.");
+                return 0;
+            }
+            Console.WriteLine($"ERROR: {rc.GetErrorMessage()}");
 
-            // count required reviewers and their votes
+            // the minimum number of reviewers cannot be set for a particular PR,
+            // because that depends on the branch policy.
+            // But it would be possible to add required reviewers (even a group)
+            // to the given PR.
 
-            // count votes from optional reviewers
-            //pr.Reviewers.First().Vote == 10
+            // todo: let this exe run by a build triggered by branch policy
+            // - what changes trigger the build to run again?
+            // - can we achieve that, the build runs after each vote?
 
-            return 0;
+            // - the error message can be handed over in a file or over the pipe (stderr or stdout)
+            // - another option is to emit the error as a vso command
+            // - further it is possible to add a new comment thread to the PR with this message
+
+            return 1; // this will let the build task fail
         }
 
         internal static void Console_Dump(string title, object data)
---
+++ ReviewerCounter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.TeamFoundation.SourceControl.WebApi;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace review_evaluation_tool
+{
+    public class ReviewerCounter
+    {
+        private readonly GitHttpClient git;
+        private readonly int minCountsRequired;
+
+        public ReviewerCounter(GitHttpClient git, int minCountsRequired)
+        {
+            this.git = git;
+            this.minCountsRequired = minCountsRequired;
+        }
+
+        public int MissingReviewers { get; protected set; } = 0;
+        public string GetErrorMessage()
+        {
+            if (MissingReviewers == 0) return "";
+            return $"Only {minCountsRequired - MissingReviewers} votes are forseenable. Please add {MissingReviewers} required reviewers to fulfill the safetyness requirement for the number of reviewers.";
+        }
+
+        /// <summary>
+        /// the minimum number of votes must be ensured (quality doesn't matter)
+        /// </summary>
+        /// <param name="pr">PullRequest object</param>
+        /// <returns>true if everything fine, else false</returns>
+        public virtual async Task<bool> Check(GitPullRequest pr)
+        {
+            var ensuredVotes = pr.Reviewers
+                .Count(
+                    r => r.IsRequired        // reviewer is either required (thus a vote is enforced)
+                    || r.Vote != 0);         // or already has voted anything
+
+            MissingReviewers = minCountsRequired - ensuredVotes;
+            return MissingReviewers == 0;
+        }
+    }
+}
