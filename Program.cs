f9fe13f20f8200af930c54043b3f5632b295d643
Balazs Juhasz <balazs.juhasz@siemens-healthineers.com>
2022-01-28 11:54:01 +0100

    enable PR Id input via command line

--- Program.cs
+++ Program.cs
@@ -22,28 +22,35 @@ namespace review_evaluation_tool
             var opt = new OptionProvider(args);
 
             var tp = opt.GetParam("BUILD_TEAMPROJECT", "tp", "syngo.net");
-            var buildId = int.Parse(opt.GetParam("BUILD_BUILDID", "buildid"));
+            var buildId = int.Parse(opt.GetParam("BUILD_BUILDID", "buildid", "0"));
+            var prId = int.Parse(opt.GetParam(null, "prid", "0"));
+            var repoName = string.Empty;
 
             var tpc = new ConnectionProvider().Connect();
             var buildClient = tpc.GetClient<BuildHttpClient>();
             var git = tpc.GetClient<GitHttpClient>();
 
-            var buildData = await buildClient.GetBuildAsync(project: tp, buildId);
-            var buildParams = JsonSerializer.Deserialize<Dictionary<string, string>>(buildData.Parameters);
+            if (buildId > 0)
+            {
+                var buildData = await buildClient.GetBuildAsync(project: tp, buildId);
+                var buildParams = JsonSerializer.Deserialize<Dictionary<string, string>>(buildData.Parameters);
+                Console_Dump($"parameters of build {buildData.BuildNumber}", buildParams);
 
-            Console_Dump($"parameters of build {buildData.BuildNumber}", buildParams);
-            var prId = int.Parse(buildParams["system.pullRequest.pullRequestId"]);
-            var repoUri = buildParams["system.pullRequest.sourceRepositoryUri"];
-            var repoName = repoUri.Split('/').Last();
+                prId = int.Parse(buildParams["system.pullRequest.pullRequestId"]);
+                var repoUri = buildParams["system.pullRequest.sourceRepositoryUri"];
+                repoName = repoUri.Split('/').Last();
+            }
+            else
+            {
+                if (prId == 0) throw new Exception("Neither a build id nor a PR id is available to start with.");
+            }
             Console.WriteLine($"loading reviewer information for PR {prId}");
+            var pr = await git.GetPullRequestByIdAsync(project: tp, prId);
+            //Console_Dump($"PR {prId}", pr);
+            var reviewers = pr.Reviewers;
 
-            // this goes faster
-            var reviewers = await git.GetPullRequestReviewersAsync(project: tp, repoName, prId);
             Console_Dump("reviewers", reviewers.Select(r => new { r.DisplayName, _ = r.IsRequired ? "required" : "optional", r.Vote }));
 
-            //// this provides more information (inclusive the above)
-            //var pr = await git.GetPullRequestByIdAsync(project: tp, prId);
-            //Console_Dump($"PR {prId}", pr);
 
             // count required reviewers and their votes
 
@@ -55,9 +62,10 @@ namespace review_evaluation_tool
 
         private static void Console_Dump(string title, object data)
         {
-            Console.WriteLine($"--- {title} {new StringBuilder().Insert(0, "-", 200 - 5 - title.Length)}");
+            const int LENGTH = 180;
+            Console.WriteLine($"--- {title} {new StringBuilder().Insert(0, "-", LENGTH - 5 - title.Length)}");
             Console.WriteLine(JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true }));
-            Console.WriteLine(new StringBuilder().Insert(0, "-", 200));
+            Console.WriteLine(new StringBuilder().Insert(0, "-", LENGTH));
         }
 
     }
